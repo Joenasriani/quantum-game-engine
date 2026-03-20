d3bd0d1f2443e84a53f2d5b195fc9d3d7c737068
JOE NASR <joenasr@gmail.com>
2026-03-20 18:38:15 +0200

    Create Engine.ts file for main game engine class

---
+++ src/core/Engine.ts
@@ -0,0 +1,21 @@
+class Engine {
+    constructor() {
+        // Initialize game state
+    }
+
+    start() {
+        // Logic to start the game
+    }
+
+    update() {
+        // Logic for updating game state
+    }
+
+    render() {
+        // Logic for rendering the game
+    }
+
+    // Other game engine methods...
+}
+
+export default Engine;
