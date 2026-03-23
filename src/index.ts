78c576b048e271435079fee80c43c8cc8be5b694
JOE NASR <joenasr@gmail.com>
2026-03-23 11:49:51 +0200

    Creating src/index.ts file to export Engine, Scene, and GameObject classes.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Exporting Engine, Scene, and GameObject classes
+
+export class Engine {
+    // Implementation of Engine class
+}
+
+export class Scene {
+    // Implementation of Scene class
+}
+
+export class GameObject {
+    // Implementation of GameObject class
+}
